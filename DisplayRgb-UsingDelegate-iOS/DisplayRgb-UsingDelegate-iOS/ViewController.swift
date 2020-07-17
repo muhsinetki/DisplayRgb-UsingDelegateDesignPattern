61e24d6d3215e95c95b8fba5c0f4538d69d10ddf
Muhsin Etki <muhsinetki@boniglobal.com>
2020-07-17 15:00:07 +0300

    Inital setup
    The application has been created

---
+++ DisplayRgb-UsingDelegate-iOS/DisplayRgb-UsingDelegate-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisplayRgb-UsingDelegate-iOS
+//
+//  Created by Muhsin Etki on 17.07.2020.
+//  Copyright © 2020 Muhsin Etki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
